fd262956839510880726f813e0d76afc7bf98dcb
levilab <levi.devhaus@gmail.com>
2025-11-27 09:44:09 +0700

    update

--- part2/course-content/src/main.jsx
+++ part2/course-content/src/main.jsx
@@ -2,8 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import axios from 'axios'
 
-const promise = axios.get('http://localhost:3001/notes')
-console.log(promise)
+axios.get('http://localhost:3001/notes').then(response => {
+  const notes = response.data
+  ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />
+)
+
+})
 
 const promise2 = axios.get('http://localhost:3001/foobar')
 console.log(promise2)
@@ -26,6 +30,3 @@ const notes = [
   }
 ]
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App notes={notes} />
-)
