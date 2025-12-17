d60183da31baa19d27227ba14cd11e57c9a1db52
levi <levi.devhaus@gmail.com>
2025-12-17 16:27:14 +0700

    render testing

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -30,6 +30,7 @@ const requestLogger = (request, response, next) => {
 app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+app.use(express.static('dist'))
 
 
 app.get('/', (request, response) => {
@@ -93,7 +94,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
