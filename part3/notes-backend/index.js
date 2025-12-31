33eb09bc3979d4cc9dad45cd09c38b19e4d83d27
levilab <levi.devhaus@gmail.com>
2025-12-31 10:01:49 +0700

    validation POST update

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -16,7 +16,7 @@ const requestLogger = (request, response, next) => {
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
-
+app.use(cors())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
