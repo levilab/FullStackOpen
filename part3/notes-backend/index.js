df34abd2155ce8805a8448232e6fd95ea44deebf
levilab <levi.devhaus@gmail.com>
2025-12-31 09:37:32 +0700

    validation POST update

--- part3/notes-backend/index.js
+++ part3/notes-backend/index.js
@@ -51,24 +51,21 @@ app.delete('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
 
     const body = request.body
     
-    if (!body.content) {
-        return response.status(400).json({
-            error: 'content missing'
-        })
-    }
 
     const note = new Note ({
         content: body.content,
         important: body.important || false,
     })
 
-    note.save().then(savedNote => {
+    note.save()
+      .then(savedNote => {
       response.json(savedNote)
     })
+      .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -101,7 +98,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({error: error.message})
+  }
 
   next(error)
 }
